5d92856faa21342c311281cfeb84e6a927913f40
Paul Cernek <ptcernek@gmail.com>
2015-01-28 13:41:12 -0800

    Screwed wit teh filez

--- src/com/google/gwt/sample/stockwatcher/dummy/Dummy.java
+++ src/com/google/gwt/sample/stockwatcher/dummy/Dummy.java
@@ -2,14 +2,21 @@ package com.google.gwt.sample.stockwatcher.dummy;
 
 public class Dummy {
 	
-	private int initialValue; 
+	private int initialValue;
+	private int finalValue; 
 	
 	public Dummy(int initialValue){
 		this.initialValue = initialValue;
+		this.finalValue = finalValue;
+		
 	}
 	
 	public void Double() {
 		initialValue = initialValue + initialValue;
+	}
+	
+	public int getInitValue(){
+	    return this.initialValue;
 	}
 
 	public static void main(String[] args) {
